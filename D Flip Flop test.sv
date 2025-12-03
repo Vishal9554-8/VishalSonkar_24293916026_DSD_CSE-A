c4d6c089f08630f54a0d2bc374467412878f7a9c
Vishal9554-8 <vishalsonkar955487@ce.du.ac.in>
2025-12-03 22:28:39 +0530

    Create D Flip Flop test.sv

---
+++ D Flip Flop test.sv
@@ -0,0 +1,27 @@
+`timescale 1ns/1ps
+module tb_d_ff_neg;
+
+    reg D, CLK;
+    wire Q;
+
+    d_ff_neg uut (D, CLK, Q);
+
+    initial begin
+        $dumpfile("d_ff_neg.vcd");
+        $dumpvars(0, tb_d_ff_neg);
+    end
+
+    initial begin
+        CLK = 0;
+        forever #5 CLK = ~CLK;
+    end
+    initial begin
+        D = 0; #12;
+        D = 1; #10;
+        D = 0; #10;
+        D = 1; #10;
+        D = 0; #10;
+        $finish;
+    end
+
+endmodule
