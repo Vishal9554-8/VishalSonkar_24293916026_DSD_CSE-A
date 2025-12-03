fcb01a37010c5177eebdb83365b912aaa2fd013f
Vishal9554-8 <vishalsonkar955487@ce.du.ac.in>
2025-12-03 22:42:44 +0530

    Create SR Latch test.sv

---
+++ SR Latch test.sv
@@ -0,0 +1,22 @@
+`timescale 1ns/1ps
+module tb_sr_latch;
+
+    reg S, R;
+    wire Q, Qbar;
+
+    sr_latch uut (S, R, Q, Qbar);
+
+    initial begin
+        $dumpfile("wave.vcd");
+        $dumpvars(0, tb_sr_latch);
+    end
+
+    initial begin
+        S = 0; R = 0; #10;   
+        S = 0; R = 1; #10;   
+        S = 1; R = 0; #10; 
+        S = 1; R = 1; #10;   
+        $finish;
+    end
+
+endmodule
